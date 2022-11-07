9bc9f08db083b8e507f10648aeddb32e359f80ce
AungZawHtet <aungzawhteth10@gmail.com>
2022-11-07 23:14:51 +0900

    Page initialized.

--- src/app/ui/routes_ui.php
+++ src/app/ui/routes_ui.php
@@ -13,50 +13,38 @@ $app->get('/{id}', function (Slim\Http\ServerRequest $request, Slim\Http\Respons
     // var_dump($this->view);
     $view = Slim\Views\Twig::fromRequest($request);
     $pageId = strtoupper($args['id']);
-    $pageList = ['ORDER', 'STARTUP'];
+    $pageList = ['HOME', 'RS'];
     if (!in_array($pageId, $pageList)) {
         print('URLを見直してください！');
         return $response;
     }
-    if ($pageId == 'STARTUP') {
-        startUp($_GET['ip']);
-        return $response;
-    }
-    $category = strtoupper($_GET['category'] ?? '');
-    $categoryList = ['DRINK'];
-    $returnArr = [];
-    if (in_array($category, $categoryList)) {
-        $returnArr = [
-            'category' => $category,
-        ];
-    }
     $view->offsetSet('HtmlHelper', new \App\api\HtmlHelper);
-    return $view->render($response, $pageId . '.twig', $returnArr);
+    return $view->render($response, $pageId . '.twig', ['pageId' => $pageId]);
 });
-function createPdf()
-{
-    $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
-    $excel  = $reader->load('C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\xlstemplates\receipt1.xlsx');
-    $sheet  = $excel->getSheetByName('Sheet1');
-    $writer     = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($excel, 'Xlsx');
-    $writer->save('C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx');
-    // exec('C:/Users/aungz/Desktop/workspace/git/POS/src/app/tools/PDFtoPrinter.exe C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.pdf R01');
-    // exec('"C:\Program Files (x86)\OpenOffice 4\program\soffice.exe" -pt R01 C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx');
+// function createPdf()
+// {
+//     $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
+//     $excel  = $reader->load('C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\xlstemplates\receipt1.xlsx');
+//     $sheet  = $excel->getSheetByName('Sheet1');
+//     $writer     = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($excel, 'Xlsx');
+//     $writer->save('C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx');
+//     // exec('C:/Users/aungz/Desktop/workspace/git/POS/src/app/tools/PDFtoPrinter.exe C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.pdf R01');
+//     // exec('"C:\Program Files (x86)\OpenOffice 4\program\soffice.exe" -pt R01 C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx');
     
-    // exec('"C:\Users\aungz\AppData\Local\Kingsoft\WPS Office\11.2.0.10426\office6\et.exe" -p C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx');
-    // exec('"C:\Program Files\LibreOffice\program\scalc.exe" -p C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx');
-}
-function startUp($ipAddress)
-{
-    $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
-    $excel  = $reader->load('C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\xlstemplates\order1.xlsx');
-    $sheet  = $excel->getSheetByName('Sheet1');
-    $sheet->setCellValue('A1', $ipAddress . ':8888/LOGIN');
-    $sheet->getStyle('A1')->getFont()->setSize(20);
-    $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($excel, 'Xlsx');
-    $file = 'C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\startUp.xlsx';
-    $writer->save($file);
-    $commandPrint = '"C:\Program Files (x86)\OpenOffice 4\program\soffice.exe" -pt R01 ' . $file;
-    exec($commandPrint);
-    return;
-}
+//     // exec('"C:\Users\aungz\AppData\Local\Kingsoft\WPS Office\11.2.0.10426\office6\et.exe" -p C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx');
+//     // exec('"C:\Program Files\LibreOffice\program\scalc.exe" -p C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\temp.xlsx');
+// }
+// function startUp($ipAddress)
+// {
+//     $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
+//     $excel  = $reader->load('C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\xlstemplates\order1.xlsx');
+//     $sheet  = $excel->getSheetByName('Sheet1');
+//     $sheet->setCellValue('A1', $ipAddress . ':8888/LOGIN');
+//     $sheet->getStyle('A1')->getFont()->setSize(20);
+//     $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($excel, 'Xlsx');
+//     $file = 'C:\Users\aungz\Desktop\workspace\git\POS\src\app\Xls\temp\startUp.xlsx';
+//     $writer->save($file);
+//     $commandPrint = '"C:\Program Files (x86)\OpenOffice 4\program\soffice.exe" -pt R01 ' . $file;
+//     exec($commandPrint);
+//     return;
+// }
